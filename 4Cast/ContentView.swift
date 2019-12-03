f8c41c5935b4afe0941f7ac4d706294a389d3724
Dylan MacFarlane <dylan@macfarlanehome.com>
2019-12-03 09:27:52 -0800

    Initial Commit

---
+++ 4Cast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  4Cast
+//
+//  Created by Dylan MacFarlane on 12/3/19.
+//  Copyright © 2019 Dylan MacFarlane. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
